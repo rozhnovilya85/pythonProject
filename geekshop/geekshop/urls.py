c4c72a73f52457e767827e83916c740614d92605
rozhnovilya85 <rozhnovilya85@gmail.com>
2021-11-17 23:13:16 +0500

    First_Homework

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from mainapp import views as mainapp
+
+
 
 urlpatterns = [
+
+    path('', mainapp.index),
+    path('products/', mainapp.products),
+    path('contact/', mainapp.contact),
+
     path('admin/', admin.site.urls),
 ]
