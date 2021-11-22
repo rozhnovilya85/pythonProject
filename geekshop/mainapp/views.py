6a886678a60062d642dbe4f91fc8009765a16f92
rozhnovilya85 <rozhnovilya85@gmail.com>
2021-11-23 01:10:18 +0500

    Homework №2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -5,8 +5,54 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainapp/index.html')
 
+links_menu = [
+    {'href': 'products', 'name': 'Все'},
+    {'href': 'products_home', 'name': 'Дом'},
+    {'href': 'products_modern', 'name': 'Модерн'},
+    {'href': 'products_office', 'name': 'Офис'},
+    {'href': 'products_classic', 'name': 'Классика'},
+]
+
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_home(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_modern(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_office(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_classic(request):
+    context = {
+        'links_menu': links_menu,
+        'title': 'Товары'
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
